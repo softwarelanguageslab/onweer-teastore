4c82fafb9aeda3d4ffe2a6b401c9839e2dbc735e
Gilles Coremans <gilles.coremans@gmail.com>
2025-01-20 14:30:05 +0100

    Changes to add onweer-agent

---
+++ rebuild_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -euxo pipefail
+
+pushd utilities/tools.descartes.teastore.dockerbase;
+
+docker build -t descartesresearch/teastore-base .;
+
+popd;
+pushd tools;
+
+./build_docker.sh;
+
+popd;
+
+docker-compose -f examples/docker/docker-compose_default.yaml build;
+
+docker ps -a | tail -n +2 | awk '{ print $1 }' | xargs -L 1000 -P 100 docker rm -f
+
