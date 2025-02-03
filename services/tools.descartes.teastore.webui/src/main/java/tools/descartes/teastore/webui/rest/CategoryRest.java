2bd6caa710b51f7ad217a8d6b26ef5d60665b99d
Gilles Coremans <gilles.coremans@gmail.com>
2025-02-03 15:36:07 +0100

    Split out 'category' and 'products' endpoints

--- services/tools.descartes.teastore.webui/src/main/java/tools/descartes/teastore/webui/rest/CategoryRest.java
+++ services/tools.descartes.teastore.webui/src/main/java/tools/descartes/teastore/webui/rest/CategoryRest.java
@@ -3,34 +3,16 @@ package tools.descartes.teastore.webui.rest;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 import tools.descartes.teastore.entities.Category;
-import tools.descartes.teastore.entities.Product;
 import tools.descartes.teastore.registryclient.Service;
 import tools.descartes.teastore.registryclient.rest.LoadBalancedCRUDOperations;
-import tools.descartes.teastore.registryclient.rest.LoadBalancedImageOperations;
-
-import java.util.HashMap;
-import java.util.List;
 
 @Path("category")
 @Produces({ "application/json" })
 public class CategoryRest {
     @GET
-    public static Response get(@QueryParam("category") Long cid) {
-        if(cid != null) {
-            List<Product> productlist = LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE,
-                    "products", Product.class, "category", cid, 0, 25);
-
-            // TODO: pagination?
-
-            HashMap<String, Object> payload = new HashMap<>();
-            payload.put("products", productlist);
-            payload.put("productImages", LoadBalancedImageOperations.getProductPreviewImages(productlist));
-            return Response.ok().entity(payload).build();
-        } else {
-            return Response.ok().entity(LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE, "categories", Category.class, -1, -1)).build();
-        }
+    public static Response get() {
+        return Response.ok().entity(LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE, "categories", Category.class, -1, -1)).build();
     }
 }
---
+++ services/tools.descartes.teastore.webui/src/main/java/tools/descartes/teastore/webui/rest/ProductsRest.java
@@ -0,0 +1,26 @@
+package tools.descartes.teastore.webui.rest;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import tools.descartes.teastore.entities.Product;
+import tools.descartes.teastore.registryclient.Service;
+import tools.descartes.teastore.registryclient.rest.LoadBalancedCRUDOperations;
+import tools.descartes.teastore.registryclient.rest.LoadBalancedImageOperations;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Path("products")
+@Produces({ "application/json" })
+public class ProductsRest {
+    @GET
+    public static Response get(@QueryParam("category") Long cid) {
+        List<Product> productlist = LoadBalancedCRUDOperations.getEntities(Service.PERSISTENCE,
+                "products", Product.class, "category", cid, 0, 25);
+
+        HashMap<String, Object> payload = new HashMap<>();
+        payload.put("products", productlist);
+        payload.put("productImages", LoadBalancedImageOperations.getProductPreviewImages(productlist));
+        return Response.ok().entity(payload).build();
+    }
+}
